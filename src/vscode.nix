47bb88da6231bd9676b902d2f046658a91f3539b
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-05-07 08:36:34 +0900

    add nix ide tools

--- src/vscode.nix
+++ src/vscode.nix
@@ -1,5 +1,11 @@
-{ config, ... }: {
+{ config, pkgs, ... }:
+{
   programs.vscode.enable = true;
+
+  home.packages = with pkgs; [
+    nixfmt
+    nixd
+  ];
 
   systemd.user.services."vscode" = {
     Unit.Description = "VsCode Server";
@@ -8,6 +14,6 @@
     Service.ExecStart = ''/run/current-system/sw/bin/bash -ic "code serve-web --host 0.0.0.0 --port 8000 --without-connection-token"'';
     Service.Restart = "always";
     Service.RuntimeMaxSec = 86400;
-    Install.WantedBy = ["default.target"];
+    Install.WantedBy = [ "default.target" ];
   };
 }
