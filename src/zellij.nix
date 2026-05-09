b8bd3e031c0f32eb2196306d4ec1980f9d2ec188
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-05-09 18:25:04 +0900

    zellij config

--- src/zellij.nix
+++ src/zellij.nix
@@ -1,5 +1,12 @@
 { ... }: {
   programs.zellij = {
     enable = true;
+    settings = {
+      default_mode = "locked";
+      mirror_session = true;
+      session_name = "main";
+      attach_to_session = true;
+      serialization_interval = 600;
+    };
   };
 }
