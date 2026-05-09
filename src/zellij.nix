493b05a557471c3b7dd9db67ae6bb2d03873501c
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-05-09 19:17:43 +0900

    set nushell as default shell in zellij

--- src/zellij.nix
+++ src/zellij.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       default_mode = "locked";
+      default_shell = "nu";
       mirror_session = true;
       session_name = "main";
       attach_to_session = true;
