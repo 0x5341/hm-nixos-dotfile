5c98bbbea7095cec69e6719a420c4ed7d92cc2fe
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-05-09 20:28:06 +0900

    add catppuccin & enable

--- flake.nix
+++ flake.nix
@@ -5,10 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    catppuccin.url = "github:catppuccin/nix";
+    catppuccin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      catppuccin,
+      ...
+    }:
     {
       nixosConfigurations = {
         "nixos" = nixpkgs.lib.nixosSystem {
@@ -19,6 +26,14 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.sharedModules = [
+                {
+                  imports = [ catppuccin.homeModules.catppuccin ];
+                  catppuccin.enable = true;
+                  catppuccin.accent = "sky";
+                  catppuccin.flavor = "frappe";
+                }
+              ];
               home-manager.users."0x5341" = ./0x5341.nix;
 
               # Optionally, use home-manager.extraSpecialArgs to pass
