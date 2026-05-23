c60570b57d7e2b4d43e1dbbda24ba7d18fc52796
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-05-23 15:02:49 +0900

    remove mesa

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -83,7 +83,6 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    mesa
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
