a183b78ecbe0bcb2a82561220a68f338dce225ba
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-06-16 18:05:39 +0900

    move copilot to new file and introduce opencode

--- src/default.nix
+++ src/default.nix
@@ -3,12 +3,14 @@
   imports = [
     # ./vnc
     ./context-mode.nix
+    ./copilot.nix
     ./direnv.nix
     ./fresh.nix
     ./general.nix
     ./gh.nix
     ./git.nix
     ./nushell.nix
+    ./opencode.nix
     ./vscode.nix
     ./zellij.nix
   ];
--- src/general.nix
+++ src/general.nix
@@ -6,57 +6,6 @@
   ];
   programs.bash.enable = true;
   programs.zoxide.enable = true;
-  programs.github-copilot-cli = {
-    enable = true;
-    package = pkgs.github-copilot-cli.overrideAttrs (
-      final: prev: {
-        version = "1.0.61";
-        src = pkgs.fetchurl {
-          url = "https://github.com/github/copilot-cli/releases/download/v${final.version}/github-copilot-${final.version}.tgz";
-          hash = "sha256-8Lks8lHa5XF9ZrC+fU/9VlzD1W32MbRZ7PZtL5YWLTA=";
-        };
-      }
-    );
-    enableMcpIntegration = true;
-    mcpServers = {
-      "context-mode" = {
-        type = "local";
-        command = "context-mode";
-        tools = ["*"];
-      };
-    };
-    settings = {
-      hooks = {
-        PreToolUse = [
-          {
-            type = "command";
-            command = "context-mode hook vscode-copilot pretooluse";
-          }
-        ];
-
-        PostToolUse = [
-          {
-            type = "command";
-            command = "context-mode hook vscode-copilot posttooluse";
-          }
-        ];
-
-        PreCompact = [
-          {
-            type = "command";
-            command = "context-mode hook vscode-copilot precompact";
-          }
-        ];
-
-        SessionStart = [
-          {
-            type = "command";
-            command = "context-mode hook vscode-copilot sessionstart";
-          }
-        ];
-      };
-    };
-  };
   programs.antigravity-cli = {
     enable = true;
   };
