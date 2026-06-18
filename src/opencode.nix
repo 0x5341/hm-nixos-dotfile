610fb0c58aa3e4635074453a9a2d9f058487c711
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-06-18 20:55:45 +0900

    add plugin to opencode
    direnv
    context-mode

--- src/opencode.nix
+++ src/opencode.nix
@@ -1,4 +1,4 @@
-{pkgs,...}:{
+{pkgs,context-mode, ...}:{
   programs.opencode = {
     enable = true;
     web.enable = true;
@@ -6,5 +6,10 @@
       "--port" "4000"
       "--hostname" "0.0.0.0"
     ];
+    settings = {
+      plugin = ["@simonwjackson/opencode-direnv" "context-mode"];
+    };
+    context = (builtins.readFile (context-mode + "/configs/opencode/AGENTS.md"));
   };
+  home.packages = [ pkgs.bun ];
 }
